34cf8f964da7325da1992868439dc6d6db1732a8
Andrei Moiseenko <moiseenko.tech@gmail.com>
2026-01-02 23:58:29 +0200

    Add solution for p0961.

--- problems/0961-n-repeated-element-in-size-2n-array/solution.go
+++ problems/0961-n-repeated-element-in-size-2n-array/solution.go
@@ -7,5 +7,17 @@ package p0961
 // URL: https://leetcode.com/problems/n-repeated-element-in-size-2n-array/
 
 func repeatedNTimes(nums []int) int {
+	// The array contains N+1 distinct values, where one value is repeated N times.
+	// Since the total length is 2N, the remaining N values appear exactly once.
+	// Therefore, the element that occurs more than once is the target.
+	seen := make(map[int]bool)
+
+	for _, x := range nums {
+		if seen[x] {
+			return x
+		}
+		seen[x] = true
+	}
+
 	return 0
 }
