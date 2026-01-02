cc9c7a961175063a38b9c73cf1fa82bc9a9df65d
Andrei Moiseenko <moiseenko.tech@gmail.com>
2026-01-02 23:46:04 +0200

    Add skeleton for p0961.

---
+++ problems/0961-n-repeated-element-in-size-2n-array/solution.go
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+// Copyright (c) 2026 Andrei Moiseenko
+
+package p0961
+
+// Problem: N-Repeated Element in Size 2N Array
+// URL: https://leetcode.com/problems/n-repeated-element-in-size-2n-array/
+
+func repeatedNTimes(nums []int) int {
+	return 0
+}
---
+++ problems/0961-n-repeated-element-in-size-2n-array/solution_test.go
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: MIT
+// Copyright (c) 2026 Andrei Moiseenko
+
+package p0961
+
+import (
+	"testing"
+)
+
+var tests = map[string]struct {
+	nums   []int
+	result int
+}{
+	"1-2-3-3": {
+		nums:   []int{1, 2, 3, 3},
+		result: 3,
+	},
+	"2-1-2-5-3-2": {
+		nums:   []int{2, 1, 2, 5, 3, 2},
+		result: 2,
+	},
+	"5-1-5-2-5-3-5-4": {
+		nums:   []int{5, 1, 5, 2, 5, 3, 5, 4},
+		result: 5,
+	},
+}
+
+func TestRepeatedNTimes(t *testing.T) {
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+			got := repeatedNTimes(test.nums)
+			expected := test.result
+			if got != expected {
+				t.Fatalf("repeatedNTimes(nums=%v) returned: %v; expected: %v",
+					test.nums, got, expected)
+			}
+		})
+	}
+}
