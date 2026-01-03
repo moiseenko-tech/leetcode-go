148acc0165e4be64dbaacd0c50d1aa9305b8632d
Andrei Moiseenko <moiseenko.tech@gmail.com>
2026-01-03 23:21:38 +0200

    Add skeleton for p0050.

---
+++ problems/0050-powx-n/solution.go
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+// Copyright (c) 2025 Andrei Moiseenko
+
+package p0050
+
+// Problem: Pow(x, n)
+// URL: https://leetcode.com/problems/powx-n/
+
+func myPow(x float64, n int) float64 {
+	return 0
+}
---
+++ problems/0050-powx-n/solution_test.go
@@ -0,0 +1,44 @@
+// SPDX-License-Identifier: MIT
+// Copyright (c) 2026 Andrei Moiseenko
+
+package p0050
+
+import (
+	"testing"
+)
+
+var tests = map[string]struct {
+	x      float64
+	n      int
+	result float64
+}{
+	"2.0-10": {
+		x:      2.0,
+		n:      10,
+		result: 1024.0,
+	},
+	"2.1-3": {
+		x:      2.1,
+		n:      3,
+		result: 9.261,
+	},
+	"2.0--2": {
+		x:      2.0,
+		n:      -2,
+		result: 0.25,
+	},
+}
+
+func TestMyPow(t *testing.T) {
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+			got := myPow(test.x, test.n)
+			expected := test.result
+			if got != expected {
+				t.Fatalf("rmyPow(x=%v, n=%v) returned: %v; expected: %v",
+					test.x, test.n, got, expected)
+			}
+		})
+	}
+}
