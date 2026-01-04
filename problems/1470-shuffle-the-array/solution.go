afa68787f23352642b2d6f14b555e592ed81ec7e
Andrei Moiseenko <moiseenko.tech@gmail.com>
2026-01-04 23:11:01 +0200

    Add naive solution for p1470.

--- problems/1470-shuffle-the-array/solution.go
+++ problems/1470-shuffle-the-array/solution.go
@@ -7,5 +7,14 @@ package p1470
 // URL: https://leetcode.com/problems/shuffle-the-array/
 
 func shuffle(nums []int, n int) []int {
-	return []int{}
+	// TODO(amoiseenko): Improve performance.
+
+	result := make([]int, 2*n)
+
+	for i := range n {
+		result[i*2] = nums[i]
+		result[i*2+1] = nums[n+i]
+	}
+
+	return result
 }
