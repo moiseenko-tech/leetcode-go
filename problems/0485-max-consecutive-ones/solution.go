a484fc6f17e387fb77356001cad8d456d23bee49
Andrei Moiseenko <moiseenko.tech@gmail.com>
2026-01-04 22:42:47 +0200

    Add solution for p0485.

--- problems/0485-max-consecutive-ones/solution.go
+++ problems/0485-max-consecutive-ones/solution.go
@@ -7,5 +7,19 @@ package p0485
 // URL: https://leetcode.com/problems/max-consecutive-ones/
 
 func findMaxConsecutiveOnes(nums []int) int {
-	return 0
+	curLen := 0
+	maxLen := 0
+
+	for _, x := range nums {
+		if x != 1 {
+			curLen = 0
+			continue
+		}
+		curLen++
+		if curLen > maxLen {
+			maxLen = curLen
+		}
+	}
+
+	return maxLen
 }
