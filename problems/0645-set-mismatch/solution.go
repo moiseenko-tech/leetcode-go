0c68b14172294f0a4f1ad2513f9dd2fcca5046e7
Andrei Moiseenko <moiseenko.tech@gmail.com>
2026-01-04 23:18:48 +0200

    Add naive solution for p0645.

--- problems/0645-set-mismatch/solution.go
+++ problems/0645-set-mismatch/solution.go
@@ -7,5 +7,23 @@ package p0645
 // URL: https://leetcode.com/problems/set-mismatch/
 
 func findErrorNums(nums []int) []int {
-	return []int{}
+	seen := make(map[int]bool)
+	var double int
+	var absent int
+
+	for _, x := range nums {
+		if seen[x] {
+			double = x
+		}
+		seen[x] = true
+	}
+
+	for i := 1; i <= len(nums); i++ {
+		if !seen[i] {
+			absent = i
+			break
+		}
+	}
+
+	return []int{double, absent}
 }
