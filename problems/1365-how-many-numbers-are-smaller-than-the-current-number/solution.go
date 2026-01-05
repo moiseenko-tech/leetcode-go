24e1c01fb782d45ebf4babccb67f6fb73b5b7360
Andrei Moiseenko <moiseenko.tech@gmail.com>
2026-01-05 18:03:10 +0200

    Add naive solution for p1365.

--- problems/1365-how-many-numbers-are-smaller-than-the-current-number/solution.go
+++ problems/1365-how-many-numbers-are-smaller-than-the-current-number/solution.go
@@ -7,8 +7,21 @@ package p1365
 // URL: https://leetcode.com/problems/how-many-numbers-are-smaller-than-the-current-number/
 
 func smallerNumbersThanCurrent(nums []int) []int {
+	// TODO(amoiseenko): Improve performance.
+	return naiveSmallerNumbersThanCurrent(nums)
+}
+
+func naiveSmallerNumbersThanCurrent(nums []int) []int {
 	n := len(nums)
 	result := make([]int, n)
+
+	for curr := range n {
+		for neigh := range n {
+			if nums[neigh] < nums[curr] {
+				result[curr]++
+			}
+		}
+	}
 
 	return result
 }
