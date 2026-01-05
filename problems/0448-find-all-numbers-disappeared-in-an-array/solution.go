9b2a2d23bfee43458dd089fdb111290115d27d2e
Andrei Moiseenko <moiseenko.tech@gmail.com>
2026-01-05 23:15:53 +0200

    Add naive solution for p0448.

--- problems/0448-find-all-numbers-disappeared-in-an-array/solution.go
+++ problems/0448-find-all-numbers-disappeared-in-an-array/solution.go
@@ -7,7 +7,20 @@ package p0448
 // URL: https://leetcode.com/problems/find-all-numbers-disappeared-in-an-array/
 
 func findDisappearedNumbers(nums []int) []int {
+	// TODO(amoiseenko): Improve performance.
+	n := len(nums)
+
+	seen := make([]bool, n)
+	for _, v := range nums {
+		seen[v-1] = true
+	}
+
 	result := make([]int, 0)
+	for i := range n {
+		if !seen[i] {
+			result = append(result, i+1)
+		}
+	}
 
 	return result
 }
