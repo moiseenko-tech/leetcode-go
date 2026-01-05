49bae8253c4063beb2134591d861b891a8670bac
Andrei Moiseenko <moiseenko.tech@gmail.com>
2026-01-05 19:41:32 +0200

    Improve solution for p1365.

--- problems/1365-how-many-numbers-are-smaller-than-the-current-number/solution.go
+++ problems/1365-how-many-numbers-are-smaller-than-the-current-number/solution.go
@@ -3,24 +3,44 @@
 
 package p1365
 
+import "fmt"
+
 // Problem: How Many Numbers Are Smaller Than the Current Number
 // URL: https://leetcode.com/problems/how-many-numbers-are-smaller-than-the-current-number/
 
 func smallerNumbersThanCurrent(nums []int) []int {
-	// TODO(amoiseenko): Improve performance.
-	return naiveSmallerNumbersThanCurrent(nums)
-}
-
-func naiveSmallerNumbersThanCurrent(nums []int) []int {
+	var maxNum int = 100
 	n := len(nums)
-	result := make([]int, n)
 
-	for curr := range n {
-		for neigh := range n {
-			if nums[neigh] < nums[curr] {
-				result[curr]++
-			}
-		}
+	freq := make([]int, maxNum+1)
+	for _, num := range nums {
+		freq[num]++
+	}
+
+	/*
+		nums:   8, 1, 2, 2, 3
+		result: 4, 0, 1, 1, 3
+
+		v = nums[i] 		// nums[0] = 8
+		result[i] = lessThan[v] // result[0] = lessThan[8]
+
+		values:   0, 1, 2, 3, 4, 5, 6, 7, 8, 9
+		freq:     0, 1, 2, 1, 0, 0, 0, 0, 1, 0
+		lessThan: 0, 0, 1, 3, 4, 4, 4, 4, 4, 5
+
+		lessThan[1] = lessThan[0] + freq[0]
+	*/
+
+	lessThan := make([]int, maxNum+1)
+	for v := 1; v < maxNum+1; v++ {
+		lessThan[v] = lessThan[v-1] + freq[v-1]
+	}
+
+	fmt.Printf("lessThan=%v", lessThan)
+
+	result := make([]int, n)
+	for i := range n {
+		result[i] = lessThan[nums[i]]
 	}
 
 	return result
