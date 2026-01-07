06a90124194102650db032ce4aa406334beced3e
Andrei Moiseenko <moiseenko.tech@gmail.com>
2026-01-07 20:31:33 +0200

    Fix test results.

--- problems/0150-evaluate-reverse-polish-notation/solution_test.go
+++ problems/0150-evaluate-reverse-polish-notation/solution_test.go
@@ -13,11 +13,11 @@ var tests = map[string]struct {
 }{
 	"2-1-add-3-mul": {
 		tokens: []string{"2", "1", "+", "3", "*"},
-		result: 2,
+		result: 9,
 	},
 	"4-13-5-div-add": {
 		tokens: []string{"4", "13", "5", "/", "+"},
-		result: 3,
+		result: 6,
 	},
 	"10-6-9-3-add--11-mul-div-mul-17-add-5-add": {
 		tokens: []string{"10", "6", "9", "3", "+", "-11", "*", "/", "*", "17", "+", "5", "+"},
