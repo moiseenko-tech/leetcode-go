e0b53eb5f03551f5be174c115a25333880953dab
Andrei Moiseenko <moiseenko.tech@gmail.com>
2026-01-07 21:04:34 +0200

    Add solution for p0150.

--- problems/0150-evaluate-reverse-polish-notation/solution.go
+++ problems/0150-evaluate-reverse-polish-notation/solution.go
@@ -3,9 +3,50 @@
 
 package p0150
 
+import (
+	"strconv"
+)
+
 // Problem: Evaluate Reverse Polish Notation
 // URL: https://leetcode.com/problems/evaluate-reverse-polish-notation/
 
 func evalRPN(tokens []string) int {
-	return 0
+	const maxTokensLen = 10000
+	const stackDepth = (maxTokensLen + 1) >> 1
+
+	var operands [stackDepth]int
+	var x, y int
+	stackSize := 0
+
+	for _, token := range tokens {
+		switch token {
+		// Operators
+		case "+":
+			x = operands[stackSize-2]
+			y = operands[stackSize-1]
+			stackSize--
+			operands[stackSize-1] = x + y
+		case "-":
+			x = operands[stackSize-2]
+			y = operands[stackSize-1]
+			stackSize--
+			operands[stackSize-1] = x - y
+		case "*":
+			x = operands[stackSize-2]
+			y = operands[stackSize-1]
+			stackSize--
+			operands[stackSize-1] = x * y
+		case "/":
+			x = operands[stackSize-2]
+			y = operands[stackSize-1]
+			stackSize--
+			operands[stackSize-1] = int(x / y)
+		// Operand
+		default:
+			operands[stackSize], _ = strconv.Atoi(token)
+			stackSize++
+		}
+	}
+
+	return operands[0]
 }
