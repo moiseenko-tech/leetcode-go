35ea2ba3faf344bbbc63b98e6c6d530a8cc1de85
Andrei Moiseenko <moiseenko.tech@gmail.com>
2026-01-21 22:38:14 +0200

    Add skeleton for p0020.

---
+++ problems/0020-valid-parentheses/solution.go
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+// Copyright (c) 2026 Andrei Moiseenko
+
+package p0020
+
+// Problem: Valid Parentheses
+// URL: https://leetcode.com/problems/valid-parentheses/
+
+func isValid(s string) bool {
+	return false
+}
---
+++ problems/0020-valid-parentheses/solution_test.go
@@ -0,0 +1,46 @@
+// SPDX-License-Identifier: MIT
+// Copyright (c) 2026 Andrei Moiseenko
+
+package p0020
+
+import "testing"
+
+var tests = map[string]struct {
+	s      string
+	result bool
+}{
+	"()--true": {
+		s:      "()",
+		result: true,
+	},
+	"()[]{}--true": {
+		s:      "()[]{}",
+		result: true,
+	},
+	"(]--false": {
+		s:      "(]",
+		result: false,
+	},
+	"([])--true": {
+		s:      "([])",
+		result: true,
+	},
+	"([)]--false": {
+		s:      "([)]",
+		result: false,
+	},
+}
+
+func TestIsValid(t *testing.T) {
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+			got := isValid(test.s)
+			expected := test.result
+			if got != expected {
+				t.Fatalf("isValid(s=%v) returned: %v; expected: %v",
+					test.s, got, expected)
+			}
+		})
+	}
+}
